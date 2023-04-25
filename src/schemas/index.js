e14e6b9f0e9b482918ddab561babfc496d881af0
Fabricio Lukestik <fabri.lkt@gmail.com>
2023-04-25 13:49:57 -0300

    Fix date

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -4,7 +4,7 @@ export const clientSchema = yup.object().shape({
   firstName: yup.string().required('This field is requiredv'),
   lastName: yup.string().required('This field is required'),
   birthDate: yup
-    .string()
+    .date()
     .required('This field is required')
     .max(new Date(), 'The date of birth must be before the actual date'),
   age: yup.number().positive().integer().required('Required'),
